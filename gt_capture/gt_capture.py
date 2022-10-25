4c029dc2bc5da3434f297d3631c69b764064500c
madspete <madsp17@student.sdu.dk>
2022-10-25 10:52:33 +0200

    Added script to get hsv values, which can make the thresholding better

--- gt_capture/gt_capture.py
+++ gt_capture/gt_capture.py
@@ -6,7 +6,7 @@ from random import random
 def setup_ssh():
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect("raspberrypi.local", username="pi", password="123")
+    ssh.connect("raspberrypi.local", username="pi", password="pi")
     return ssh
 
 def get_image(shh, remote_path="/home/pi/image.jpg", local_path="image.jpg"):
@@ -17,21 +17,16 @@ def get_image(shh, remote_path="/home/pi/image.jpg", local_path="image.jpg"):
 def get_xy(image):
     # TODO find the homography based on local measurements, meaning find the world coodinates and the pixels coordinates, 
     # then the homography can map the circle coordinate sto the real world coordinates
-    image = cv2.medianBlur(image,5)
+    #image = cv2.medianBlur(image,5)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    print("gray image")
-    cv2.imshow("circle image ", gray_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    circles_img = cv2.HoughCircles(gray_image,cv2.HOUGH_GRADIENT,1,200,param1=50,param2=40,minRadius=40,maxRadius=140)
-    if circles_img is None:
-        print("No circles found")
-    else:
-        print("found circles")
-        circles_img = np.uint16(np.around(circles_img))
-        for i in circles_img[0,:]:
-            cv2.circle(image, (i[0],i[1]),i[2],(0,255,0),2)
-            cv2.circle(image,(i[0],i[1]),2,(0,0,255),3)
+    cv2.imwrite('gray_image.png', gray_image)
+    gray_image = gray_image[36:1937, 577:2120] # change when setup is changed
+    cv2.imwrite('gray_image.png', gray_image)
+
+    (thresh, im_bw) = cv2.threshold(gray_image, 70, 255,  cv2.THRESH_BINARY)
+    cv2.BackgroundSubtractorMOG2()
+    print(f"thresh is {thresh}")
+    cv2.imwrite('bw_image.png', im_bw)
 
     # TODO compute real world x and y and return
     x = 1
@@ -48,16 +43,13 @@ if __name__ == '__main__':
         command = input("Type e to exit or c to continue: ")
         if command == 'e':
             break
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("libcamera-still -o image.jpg --tuning-file /usr/share/libcamera/ipa/raspberrypi/imx219_noir.json")
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("raspistill -o image.jpg")
         
         get_image(ssh, local_path=local_path)
         image = cv2.imread(local_path)
-        cv2.imshow("image", image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
         # TODO fully implement this
-        #x,y = get_xy(image)
+        x,y = get_xy(image)
         x = random()
         y = random()
         # Write the xy coordinates to a file
