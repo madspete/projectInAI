f43ad8943f725d09c81ee2deb06d1c9d86dff965
madspete <madsp17@student.sdu.dk>
2023-01-04 11:05:36 +0100

    Changed test data

--- cnn/test_cnn.py
+++ cnn/test_cnn.py
@@ -1,8 +1,36 @@
 from cnn import CNN
-
+import csv
+import numpy as np
 
 if __name__ == '__main__':
     cnn = CNN()
-    cnn.load_model("/home/mads/git/project_in_AI/cnn/models/retina_extraction.hdf5")
+    cnn.load_model("cnn/models/retina_extraction.hdf5")
 
+    data_path_test = "data/test_data.txt"
+    targets_test_path = "data/test/test.txt"
+
+    test_data = []
+    with open(data_path_test) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=' ')
+        for row in csv_reader:
+            data = []
+            for i in range(16):
+                data.append(float(row[i]))
+            test_data.append(data)
+
+    test_data = np.array(test_data)
     
+    test_targets = []
+    with open(targets_test_path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=' ')
+        for row in csv_reader:
+            data = []
+            data.append(float(row[0]))
+            data.append(float(row[1]))
+            test_targets.append(data)
+            
+    print(test_targets)
+    test_targets = np.array(test_targets)
+
+    for i in range(len(test_targets)):
+        cnn.predict(np.array([test_data[i]]), np.array([test_targets[i]]))
